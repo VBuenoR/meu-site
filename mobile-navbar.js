e434fa03b3f53ba11587900adc6b36931bbb39b0
VBuenoR <136184345+VBuenoR@users.noreply.github.com>
2025-01-30 09:38:47 -0300

    Subir arquivos

---
+++ mobile-navbar.js
@@ -0,0 +1,59 @@
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
+    }
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+  );
+
+  mobileNavbar.init();
+  
+  let lastScrollY = window.scrollY; // Posição inicial do scroll
+  const header = document.getElementById("header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > lastScrollY) {
+    // Rolando para baixo
+    header.classList.add("hidden");
+  } else {
+    // Rolando para cima
+    header.classList.remove("hidden");
+  }
+  lastScrollY = window.scrollY; // Atualiza a última posição do scroll
+});
